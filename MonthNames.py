d651e1d25ac1bec18caf153314497e3d435ddee5
RadGit123 <radhikaps70@gmail.com>
2025-02-18 22:42:36 +0400

    Delete MonthNames.py

--- MonthNames.py
+++
@@ -1,8 +0,0 @@
-M=['January','February','March','April','May','June','July','Augest','September','October','November','December']
-
-E=int(input("Enter the Number:"))       
-
-if E>=1  and E<=12:
-  print(f"THE MONTH {E} IS {M[E-1]}")
-else:
-  print("The number is not valid")
