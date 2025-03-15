a0752678abac74a8e215289c5e3ca063f106f88d
RadGit123 <radhikaps70@gmail.com>
2025-03-15 17:41:09 +0400

    Add files via upload

---
+++ employee.py
@@ -0,0 +1,11 @@
+class Employee:
+        def __init__(self,Name,Salary):
+         self.Name=Name
+         self.Salary=Salary
+    
+        def get_name(self):
+         return self.Name
+    
+        def get_salary(self):
+         return self.Salary
+    
